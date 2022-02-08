a637f8adbf7ec89b2cd066a23ba293f8e826db7d
moussadiakite64 <moussadiakite1963@gmail.com>
2022-02-08 10:17:43 +0000

    soutenance

---
+++ src/main/java/com/myparkprojet/parkProject/ParkProjectApplication.java
@@ -0,0 +1,13 @@
+package com.myparkprojet.parkProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myparkprojet/parkProject/ParkProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myparkprojet.parkProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
